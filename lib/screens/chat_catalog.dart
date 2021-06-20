aee3a9d019948bdbf8e69ae61eb1014a70aca972
Goga Eva <igorevtushenko22@gmail.com>
2021-06-20 18:32:53 +0300

    improve context menu on chat labels

--- lib/screens/chat_catalog.dart
+++ lib/screens/chat_catalog.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:focused_menu/modals.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'package:telegrammm/models/chat.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:focused_menu/focused_menu.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class ChatCatalog extends StatelessWidget {
   @override
@@ -20,16 +23,16 @@ class ChatCatalog extends StatelessWidget {
       ),
       backgroundColor: Colors.black,
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('chat-catalog').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) return const Text("Loading...");
-          return ListView.builder(
-            itemCount: snapshot.data?.docs.length,
-            itemBuilder: (context, index) =>
-            ChatLabel.fromDb(snapshot.data?.docs[index]),
-          );
-        }
-      ),
+          stream:
+              FirebaseFirestore.instance.collection('chat-catalog').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) return const Text("Loading...");
+            return ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: (context, index) => ChatLabel.fromDb(snapshot.data?.docs[index]),
+            );
+          }),
     );
   }
 }
@@ -44,13 +47,14 @@ class ChatLabel extends StatelessWidget {
     required this.unreadCount,
   });
 
-  ChatLabel.fromDb(QueryDocumentSnapshot<Object?>? document) : this(
-    color: (Colors.pink[200])!,
-    name: document?['name'],
-    lastMessage: document?['lastMessage'],
-    pinned: document?['pinned'],
-    unreadCount: document?['unreadCount'],
-  );
+  ChatLabel.fromDb(QueryDocumentSnapshot<Object?>? document)
+      : this(
+          color: (Colors.pink[200])!,
+          name: document?['name'],
+          lastMessage: document?['lastMessage'],
+          pinned: document?['pinned'],
+          unreadCount: document?['unreadCount'],
+        );
 
   final Color color;
   final String name;
@@ -64,79 +68,81 @@ class ChatLabel extends StatelessWidget {
         create: (context) =>
             ChatModel(pinned: pinned, unreadCount: unreadCount),
         builder: (context, _) {
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundColor: color,
+          return FocusedMenuHolder(
+            menuWidth: MediaQuery.of(context).size.width * 0.50,
+            blurSize: 5.0,
+            menuItemExtent: 45,
+            menuBoxDecoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
             ),
-            title: Text(
-              name,
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            duration: Duration(milliseconds: 100),
+            animateMenuItems: true,
+            blurBackgroundColor: Colors.black54,
+            bottomOffsetHeight: 100,
+            onPressed: () {},
+            menuItems: <FocusedMenuItem>[
+              FocusedMenuItem(
+                title: context.read<ChatModel>().pinned
+                    ? Text("Pin off")
+                    : Text("Pin"),
+                trailingIcon: context.read<ChatModel>().pinned
+                    ? Icon(MdiIcons.pinOff)
+                    : Icon(MdiIcons.pin),
+                onPressed: () => context.read<ChatModel>().pin(),
+              )
+            ],
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: color,
               ),
-            ),
-            subtitle: Text(
-              lastMessage,
-              style: TextStyle(color: Colors.grey),
-            ),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  timeLabel('2021-04-09 20:00:00'),
-                  style: TextStyle(color: Colors.white),
+              title: Text(
+                name,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                if (Provider.of<ChatModel>(context).pinned)
-                  SvgPicture.asset(
-                    "assets/push-pin-svgrepo-com.svg",
-                    width: 12.0,
-                    height: 12.0,
-                    color: Colors.white,
-                  )
-                else
-                  Container(
-                    child: Text(unreadCount.toString()),
-                    decoration: ShapeDecoration(
+              ),
+              subtitle: Text(
+                lastMessage,
+                style: TextStyle(color: Colors.grey),
+              ),
+              trailing: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    timeLabel('2021-04-09 20:00:00'),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  if (Provider.of<ChatModel>(context).pinned)
+                    SvgPicture.asset(
+                      "assets/push-pin-svgrepo-com.svg",
+                      width: 12.0,
+                      height: 12.0,
                       color: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.horizontal(
-                          left: Radius.circular(7.0),
-                          right: Radius.circular(7.0),
+                    )
+                  else
+                    Container(
+                      child: Text(unreadCount.toString()),
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.horizontal(
+                            left: Radius.circular(7.0),
+                            right: Radius.circular(7.0),
+                          ),
                         ),
                       ),
-                    ),
-                    padding: EdgeInsets.all(2.0),
-                  )
-              ],
-            ),
-            onLongPress: () => showMenu(
-              context: context,
-              position: RelativeRect.fromLTRB(0.0, 0.0, 50.0, 50.0),
-              items: _menuItems(context),
+                      padding: EdgeInsets.all(2.0),
+                    )
+                ],
+              ),
             ),
           );
         });
   }
-}
-
-//TODO: Expanded
-List<PopupMenuItem<ListTile>> _menuItems(BuildContext context) {
-  var chat = context.read<ChatModel>();
-  return <PopupMenuItem<ListTile>>[
-    PopupMenuItem(
-        child: ListTile(
-      title: chat.pinned ? Text("Pin off") : Text("Pin"),
-      trailing: SvgPicture.asset(
-        chat.pinned ? "assets/options-pin-off.svg" : "assets/options-pin.svg",
-        width: 12.0,
-        height: 12.0,
-        color: Colors.black,
-      ),
-      onTap: chat.pin,
-    )),
-  ];
 }
 
 String timeLabel(String time) {
@@ -150,4 +156,3 @@ String timeLabel(String time) {
   else
     return DateFormat.Hm().format(then);
 }
-
